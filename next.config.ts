b5e0491eed52ecbd1982711363d37ec013060a2e
Deni Supriyatna <denisupriyatna01@gmail.com>
2025-05-13 16:53:49 +0700

    Partial Prerendering
    https://nextjs.org/learn/dashboard-app/partial-prerendering

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    // output: "export", // ini buat static export,
+    // ppr: "incremental", // config ini buat Partial Prerendering, jadi gabungan antara static rendering dan dinamic rendering, .
 };
 
 export default nextConfig;
