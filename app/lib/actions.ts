9dd98e5cdaf4ebef062a95b4ecf02396b4b3cc58
Deni Supriyatna <denisupriyatna01@gmail.com>
2025-05-14 16:43:07 +0700

    Chapter 12. Mutating data part 1
    https://nextjs.org/learn/dashboard-app/mutating-data

---
+++ app/lib/actions.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import { z } from "zod";
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(["pending", "paid"]),
+    date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+    const rawFormData = {
+        customerId: formData.get("customerId"),
+        amount: formData.get("amount"),
+        status: formData.get("status"),
+    };
+
+    const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formData.get("customerId"),
+        amount: formData.get("amount"),
+        status: formData.get("status"),
+    });
+
+    // test it aout
+    console.log(rawFormData);
+    console.log(typeof rawFormData.amount);
+    console.log("type");
+    console.log(`customerId = ${customerId}`);
+    console.log(`amount = ${amount} . type = ${typeof amount}`);
+    console.log(`status = ${status}`);
+}
